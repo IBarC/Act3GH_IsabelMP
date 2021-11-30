038f9636fdd4a2990ad558e39a7e137846710ac7
Irene <a_irene.barboteo.company@iespablopicasso.es>
2021-11-30 13:16:52 +0100

    Añado una impresion por pantalla

--- egit/src/egit/arraynumrandom.java
+++ egit/src/egit/arraynumrandom.java
@@ -13,5 +13,6 @@ public class arraynumrandom {
 		for (int i = 0; i < arraycont.length; i++) {
 			System.out.print("\n"+(i+1)+": "+arraycont[i]);
 		}
+		System.out.println("Hola caracola");
 	}
 }
